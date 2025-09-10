f2dcdb874adf975c2cd017be2d70b2d4d05a3c02
DarkIsDude <edouard.comtet@gmail.com>
2025-09-10 13:03:26 +0200

    🚨 add eslint configuration

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default [
+  js.configs.recommended,
+  globalIgnores(['dist/', 'node_modules/', '.yarn/', 'webpack.config.js', 'src/jslib-aws/**', 'eslint.config.js']),
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        console: false,
+        __ENV: false,
+      },
+    },
+    plugins: { '@typescript-eslint': tseslint },
+    rules: tseslint.configs.recommended.rules,
+  },
+  prettier,
+  {
+    rules: {
+      'no-console': 'off',
+    },
+  },
+];
